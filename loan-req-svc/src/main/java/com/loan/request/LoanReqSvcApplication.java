828de8fab53f42ad031c3050caa3a5cc57e159f0
bgrbalu <bgrbalu@gmail.com>
2020-11-09 21:58:31 -0500

    Loan request application

---
+++ loan-req-svc/src/main/java/com/loan/request/LoanReqSvcApplication.java
@@ -0,0 +1,13 @@
+package com.loan.request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanReqSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanReqSvcApplication.class, args);
+	}
+
+}
